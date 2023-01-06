8ae6fcdc692a3fe8bbc4d7054deca6da60ba1dc5
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-01-06 06:39:51 +0200

    Plot Demodulated Signals 📈

--- Modulation.m
+++ Modulation.m
@@ -1,3 +1,6 @@
+% Close all opened figures
+close all;
+
 % Read the input audio
 [message_1, fs_1] = audioread('Input/audio_1.m4a');
 [message_2, fs_2] = audioread('Input/audio_2.m4a');
@@ -19,12 +22,14 @@ N_3 = length(message_3);
 % Get the maximum length among them
 max_N = max([N_1, N_2, N_3]);
 
-% Adjust all lengths of the 3 input signals to be the same
+% Adjust all lengths of the 3 input signals to be the same (having max length)
 message_1 = [message_1;zeros(max_N-N_1, 1)];
 message_2 = [message_2;zeros(max_N-N_2, 1)];
 message_3 = [message_3;zeros(max_N-N_3, 1)];
 
 % Time & Frequency interval
+global time;
+global freq;
 time = linspace(0, max_N/fs, max_N);
 df = fs/2;
 freq = -df : fs/max_N: df - fs/max_N;
@@ -39,9 +44,10 @@ phase_2 = unwrap(angle(message_f2));
 phase_3 = unwrap(angle(message_f3));
 
 % Carrier Frequency
-fc_1 = 4750;
+fc_1 = 5000;
 fc_2 = 3 * fc_1;
 
+% Omega = 2piF
 wc_1 = 2*pi * fc_1;
 wc_2 = 2*pi * fc_2;
 
@@ -80,44 +86,6 @@ finalModulatedSignal = modulatedSignal_t1 + modulatedSignal_t2 + modulatedSignal
 finalModulatedSignal_f = fftshift(fft(finalModulatedSignal));
 phase_mod = unwrap(angle(finalModulatedSignal_f));
 
-% Set passband frequency
-fp = fc_1;
-
-% Synchronous Demodulation
-demodulate(finalModulatedSignal, carrier_t1, fs, fp, "result1_Sync");
-demodulate(finalModulatedSignal, carrier_t2, fs, fp, "result2_Sync");
-demodulate(finalModulatedSignal, carrier_t3, fs, fp, "result3_Sync");
-
-% Phase Shift = 10
-[carrier1_10, carrier2_10, carrier3_10] = generateCarriersWithPhase(fc_1, fc_2, time, 10);
-demodulate(finalModulatedSignal, carrier1_10, fs, fp, "result1_10");
-demodulate(finalModulatedSignal, carrier2_10, fs, fp, "result2_10");
-demodulate(finalModulatedSignal, carrier3_10, fs, fp, "result3_10");
-
-% Phase Shift = 30
-[carrier1_30, carrier2_30, carrier3_30] = generateCarriersWithPhase(fc_1, fc_2, time, 30);
-demodulate(finalModulatedSignal, carrier1_30, fs, fp, "result1_30");
-demodulate(finalModulatedSignal, carrier2_30, fs, fp, "result2_30");
-demodulate(finalModulatedSignal, carrier3_30, fs, fp, "result3_30");
-
-% Phase Shift = 90
-[carrier1_90, carrier2_90, carrier3_90] = generateCarriersWithPhase(fc_1, fc_2, time, 90);
-demodulate(finalModulatedSignal, carrier1_90, fs, fp, "result1_90");
-demodulate(finalModulatedSignal, carrier2_90, fs, fp, "result2_90");
-demodulate(finalModulatedSignal, carrier3_90, fs, fp, "result3_90");
-
-% Demodulation with local carrier frequency different than Fc by 2 Hz
-[carrier1_2Hz, carrier2_2Hz, carrier3_2Hz] = generateCarriersWithDifferentFc(fc_1, fc_2, time, 2);
-demodulate(finalModulatedSignal, carrier1_2Hz, fs, fp, "result1_2Hz");
-demodulate(finalModulatedSignal, carrier2_2Hz, fs, fp, "result2_2Hz");
-demodulate(finalModulatedSignal, carrier3_2Hz, fs, fp, "result3_2Hz");
-
-% Demodulation with local carrier frequency different than Fc by 10 Hz
-[carrier1_10Hz, carrier2_10Hz, carrier3_10Hz] = generateCarriersWithDifferentFc(fc_1, fc_2, time, 10);
-demodulate(finalModulatedSignal, carrier1_10Hz, fs, fp, "result1_10Hz");
-demodulate(finalModulatedSignal, carrier2_10Hz, fs, fp, "result2_10Hz");
-demodulate(finalModulatedSignal, carrier3_10Hz, fs, fp, "result3_10Hz");
-
 % Plot Message Signals
 draw(time, freq.', 1, message_1, abs(message_f1), phase_1, 'Message 1');
 draw(time, freq.', 2, message_2, abs(message_f2), phase_2, 'Message 2');
@@ -131,7 +99,50 @@ draw(time, freq.', 6, modulatedSignal_t3, abs(modulatedSignal_f3), phase_mod3, '
 % Plot the Final Modulated Signal
 draw(time, freq.', 7, finalModulatedSignal, abs(finalModulatedSignal_f), phase_mod, 'FINAL Modulated Signal');
 
+% Figure counter for Plotting
+global j;
+j = 8;
+
+% Set passband frequency
+fp = fc_1;
+
+% Synchronous Demodulation
+demodulate(finalModulatedSignal, carrier_t1, fs, fp, "Sync_1");
+demodulate(finalModulatedSignal, carrier_t2, fs, fp, "Sync_2");
+demodulate(finalModulatedSignal, carrier_t3, fs, fp, "Sync_3");
+
+% Phase Shift = 10
+[carrier1_10, carrier2_10, carrier3_10] = generateCarriersWithPhase(fc_1, fc_2, time, 10);
+demodulate(finalModulatedSignal, carrier1_10, fs, fp, "PhaseShift10_1");
+demodulate(finalModulatedSignal, carrier2_10, fs, fp, "PhaseShift10_2");
+demodulate(finalModulatedSignal, carrier3_10, fs, fp, "PhaseShift10_3");
+
+% Phase Shift = 30
+[carrier1_30, carrier2_30, carrier3_30] = generateCarriersWithPhase(fc_1, fc_2, time, 30);
+demodulate(finalModulatedSignal, carrier1_30, fs, fp, "PhaseShift30_1");
+demodulate(finalModulatedSignal, carrier2_30, fs, fp, "PhaseShift30_2");
+demodulate(finalModulatedSignal, carrier3_30, fs, fp, "PhaseShift30_3");
+
+% Phase Shift = 90
+[carrier1_90, carrier2_90, carrier3_90] = generateCarriersWithPhase(fc_1, fc_2, time, 90);
+demodulate(finalModulatedSignal, carrier1_90, fs, fp, "PhaseShift90_1");
+demodulate(finalModulatedSignal, carrier2_90, fs, fp, "PhaseShift90_2");
+demodulate(finalModulatedSignal, carrier3_90, fs, fp, "PhaseShift90_3");
+
+% Demodulation with local carrier frequency different than Fc by 2 Hz
+[carrier1_2Hz, carrier2_2Hz, carrier3_2Hz] = generateCarriersWithDifferentFc(fc_1, fc_2, time, 2);
+demodulate(finalModulatedSignal, carrier1_2Hz, fs, fp, "Fc2Hz_1");
+demodulate(finalModulatedSignal, carrier2_2Hz, fs, fp, "Fc2Hz_2");
+demodulate(finalModulatedSignal, carrier3_2Hz, fs, fp, "Fc2Hz_3");
+
+% Demodulation with local carrier frequency different than Fc by 10 Hz
+[carrier1_10Hz, carrier2_10Hz, carrier3_10Hz] = generateCarriersWithDifferentFc(fc_1, fc_2, time, 10);
+demodulate(finalModulatedSignal, carrier1_10Hz, fs, fp, "Fc10Hz_1");
+demodulate(finalModulatedSignal, carrier2_10Hz, fs, fp, "Fc10Hz_2");
+demodulate(finalModulatedSignal, carrier3_10Hz, fs, fp, "Fc10Hz_3");
+
 function draw(time, freq, i, mt, mf, phase, T)
+    % Plot Message in Time Domain
     figure(i)
     subplot(3, 1, 1)
     plot(time, mt)
@@ -139,12 +150,14 @@ function draw(time, freq, i, mt, mf, phase, T)
     ylabel('Amp')
     title(strcat(T, ' Signal (Time)'))
     
+    % Plot Message Amplitude in Frequency Domain
     subplot(3, 1, 2)
     plot(freq, mf)
     xlabel('Freq')
     ylabel('Amp')
     title(strcat(T, ' Signal Amplitude (Freq)'))
     
+    % Plot Message Phase in Frequency Domain
     subplot(3, 1, 3)
     plot(freq, phase)
     xlabel('Freq')
@@ -153,31 +166,49 @@ function draw(time, freq, i, mt, mf, phase, T)
 end
 
 function demodulate(signal, carrier, fs, fp, filename)
+    % Re-multiply by the carrier to restore the signal with half magnitude
+    % So we double the output to get the exact original signal
     demodulatedSignal = 2 * (signal .* carrier);
+    
+    % Low pass filter to get each message alone
     lpf = lowpass(demodulatedSignal, fp, fs);
     
+    % Get demodulated signal amplitude and phase in Frequency Domain
     lpf_f = fftshift(fft(lpf));
     phase_demod = unwrap(angle(lpf_f));
     
+    % Plot the demodulated signal
+    global time;
+    global freq;
+    global j;
+    draw(time, freq.', j, lpf, abs(lpf_f), phase_demod, filename);
+    j = j + 1;
+    
+    % Output the final result to an audio file
     audiowrite(strcat("Output/", filename, '.m4a'), lpf, fs);
 end
 
 function [carrier1, carrier2, carrier3] = generateCarriersWithPhase(fc_1, fc_2, time, deg)
-     phaseShift = (deg * pi) / 180; 
-     
-     wc_1 = 2*pi * fc_1;
-     wc_2 = 2*pi * fc_2;
-     
-     carrier1 = cos((wc_1 * time) + phaseShift);
-     carrier2 = cos((wc_2 * time) + phaseShift);
-     carrier3 = sin((wc_2 * time) + phaseShift);
+    % Phase Shift in radians
+    phaseShift = (deg * pi) / 180; 
+    
+    % Omega = 2piF
+    wc_1 = 2*pi * fc_1;
+    wc_2 = 2*pi * fc_2;
+    
+    % Get carrier for each signal with an additional phase shift
+    carrier1 = cos((wc_1 * time) + phaseShift);
+    carrier2 = cos((wc_2 * time) + phaseShift);
+    carrier3 = sin((wc_2 * time) + phaseShift);
 end
 
 function [carrier1, carrier2, carrier3] = generateCarriersWithDifferentFc(fc_1, fc_2, time, dfc)
-     wc_1 = 2*pi * (fc_1 + dfc);
-     wc_2 = 2*pi * (fc_2 + dfc);
-     
-     carrier1 = cos(wc_1 * time);
-     carrier2 = cos(wc_2 * time);
-     carrier3 = sin(wc_2 * time);
+    % Get new omega with a constant added to the carrier frequency
+    wc_1 = 2*pi * (fc_1 + dfc);
+    wc_2 = 2*pi * (fc_2 + dfc);
+    
+    % Get new carriers just like we did in synchronous demodulation
+    carrier1 = cos(wc_1 * time);
+    carrier2 = cos(wc_2 * time);
+    carrier3 = sin(wc_2 * time);
 end
